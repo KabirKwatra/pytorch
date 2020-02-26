04665f1423473ec7c4942702ad8f4aeaf9f3182c
Restyled.io <commits@restyled.io>
2020-02-26 06:46:23 +0000

    Restyled by clang-format

--- torch/csrc/jit/mobile/module.h
+++ torch/csrc/jit/mobile/module.h
@@ -7,35 +7,37 @@ namespace jit {
 namespace mobile {
 using Stack = std::vector<c10::IValue>;
 
-
 class CompilationUnit {
-public:
-    void register_function(std::unique_ptr<Function> fn);
-    std::vector<std::unique_ptr<Function>>& methods() {
-        return methods_;
-    }
-private:
-    std::vector<std::unique_ptr<Function>> methods_;
+ public:
+  void register_function(std::unique_ptr<Function> fn);
+  std::vector<std::unique_ptr<Function>>& methods() {
+    return methods_;
+  }
+
+ private:
+  std::vector<std::unique_ptr<Function>> methods_;
 };
 
 class TORCH_API Module {
-public:
-    Module(c10::intrusive_ptr<c10::ivalue::Object> object,
-           std::shared_ptr<CompilationUnit> cu)
-        : object_(object), cu_(std::move(cu)) {};
-    Module() {}
-    c10::IValue run_method(const std::string& method_name, Stack stack);
-    c10::IValue forward(std::vector<c10::IValue> inputs) {
-        return run_method("forward", std::move(inputs));
-    }
-    Function* find_method(const std::string& basename) const;
-    std::string name() {
-        return object_->name();
-    }
-private:
-    c10::intrusive_ptr<c10::ivalue::Object> object_;
-    std::shared_ptr<CompilationUnit> cu_;
+ public:
+  Module(
+      c10::intrusive_ptr<c10::ivalue::Object> object,
+      std::shared_ptr<CompilationUnit> cu)
+      : object_(object), cu_(std::move(cu)){};
+  Module() {}
+  c10::IValue run_method(const std::string& method_name, Stack stack);
+  c10::IValue forward(std::vector<c10::IValue> inputs) {
+    return run_method("forward", std::move(inputs));
+  }
+  Function* find_method(const std::string& basename) const;
+  std::string name() {
+    return object_->name();
+  }
+
+ private:
+  c10::intrusive_ptr<c10::ivalue::Object> object_;
+  std::shared_ptr<CompilationUnit> cu_;
 };
 } // namespace mobile
-} // namespace torch
 } // namespace jit
+} // namespace torch
