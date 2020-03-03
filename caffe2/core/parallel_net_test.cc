b273bfbeeea43e4f39471c705b2c8937ef59df9f
Restyled.io <commits@restyled.io>
2020-03-03 18:46:21 +0000

    Restyled by astyle

--- caffe2/core/parallel_net_test.cc
+++ caffe2/core/parallel_net_test.cc
@@ -15,29 +15,29 @@ const int kTimeThreshold = 40;
 // test scaffolding of networks. If the output is 1, it will be filled with
 // vector<int64_t> with two elements: start time and end time.
 class SleepOp final : public Operator<CPUContext> {
- public:
-  SleepOp(const OperatorDef& operator_def, Workspace* ws)
-      : Operator<CPUContext>(operator_def, ws),
-        ms_(OperatorBase::GetSingleArgument<int>("ms", 1000)) {
-    DCHECK_GT(ms_, 0);
-    DCHECK_LT(ms_, 3600 * 1000) << "Really? This long?";
-  }
-
-  bool RunOnDevice() override {
-    auto start = std::chrono::high_resolution_clock::now();
-    std::this_thread::sleep_for(std::chrono::milliseconds(ms_));
-    auto end = std::chrono::high_resolution_clock::now();
-    if (OperatorBase::OutputSize()) {
-      vector<int64_t>* output = OperatorBase::Output<vector<int64_t>>(0);
-      output->resize(2);
-      (*output)[0] = start.time_since_epoch().count();
-      (*output)[1] = end.time_since_epoch().count();
+public:
+    SleepOp(const OperatorDef& operator_def, Workspace* ws)
+        : Operator<CPUContext>(operator_def, ws),
+          ms_(OperatorBase::GetSingleArgument<int>("ms", 1000)) {
+        DCHECK_GT(ms_, 0);
+        DCHECK_LT(ms_, 3600 * 1000) << "Really? This long?";
     }
-    return true;
-  }
 
- private:
-  int ms_;
+    bool RunOnDevice() override {
+        auto start = std::chrono::high_resolution_clock::now();
+        std::this_thread::sleep_for(std::chrono::milliseconds(ms_));
+        auto end = std::chrono::high_resolution_clock::now();
+        if (OperatorBase::OutputSize()) {
+            vector<int64_t>* output = OperatorBase::Output<vector<int64_t>>(0);
+            output->resize(2);
+            (*output)[0] = start.time_since_epoch().count();
+            (*output)[1] = end.time_since_epoch().count();
+        }
+        return true;
+    }
+
+private:
+    int ms_;
 };
 
 OPERATOR_SCHEMA(Sleep).NumInputs(0, INT_MAX).NumOutputs(0, 1);
@@ -81,36 +81,36 @@ const char kSleepNetDefString[] =
 namespace {
 // Run a network and get its duration in milliseconds.
 int RunNetAndGetDuration(const string& net_def_str, const string& type) {
-  NetDef net_def;
-  CAFFE_ENFORCE(TextFormat::ParseFromString(net_def_str, &net_def));
-  net_def.set_type(type);
-  Workspace ws;
-  unique_ptr<NetBase> net(CreateNet(net_def, &ws));
-  CAFFE_ENFORCE(net.get() != nullptr);
-  // Run once to kick in potential initialization (can be slower)
-  CAFFE_ENFORCE(net->Run());
-  // Now run and time it
-  auto start_time = std::chrono::system_clock::now();
-  CAFFE_ENFORCE(net->Run());
-  // Inspect the time - it should be around 200 milliseconds, since sleep3 can
-  // run in parallel with sleep1 and sleep2.
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-      std::chrono::system_clock::now() - start_time);
-  int milliseconds = duration.count();
-  return milliseconds;
+    NetDef net_def;
+    CAFFE_ENFORCE(TextFormat::ParseFromString(net_def_str, &net_def));
+    net_def.set_type(type);
+    Workspace ws;
+    unique_ptr<NetBase> net(CreateNet(net_def, &ws));
+    CAFFE_ENFORCE(net.get() != nullptr);
+    // Run once to kick in potential initialization (can be slower)
+    CAFFE_ENFORCE(net->Run());
+    // Now run and time it
+    auto start_time = std::chrono::system_clock::now();
+    CAFFE_ENFORCE(net->Run());
+    // Inspect the time - it should be around 200 milliseconds, since sleep3 can
+    // run in parallel with sleep1 and sleep2.
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+                        std::chrono::system_clock::now() - start_time);
+    int milliseconds = duration.count();
+    return milliseconds;
 }
 } // namespace
 
 TEST(DAGNetTest, TestDAGNetTiming) {
-  int ms = RunNetAndGetDuration(string(kSleepNetDefString), "dag");
-  EXPECT_NEAR(ms, 200, kTimeThreshold);
+    int ms = RunNetAndGetDuration(string(kSleepNetDefString), "dag");
+    EXPECT_NEAR(ms, 200, kTimeThreshold);
 }
 
 // For sanity check, we also test the sequential time - it should take 0.35
 // seconds instead since everything has to be sequential.
 TEST(SimpleNetTest, TestSimpleNetTiming) {
-  int ms = RunNetAndGetDuration(string(kSleepNetDefString), "simple");
-  EXPECT_NEAR(ms, 350, kTimeThreshold);
+    int ms = RunNetAndGetDuration(string(kSleepNetDefString), "simple");
+    EXPECT_NEAR(ms, 350, kTimeThreshold);
 }
 
 // This network has two operators reading the same blob at the same time. This
@@ -151,16 +151,16 @@ const char kSleepNetDefStringReadAfterRead[] =
     "  }";
 
 TEST(DAGNetTest, TestDAGNetTimingReadAfterRead) {
-  int ms = RunNetAndGetDuration(string(kSleepNetDefStringReadAfterRead), "dag");
-  EXPECT_NEAR(ms, 250, kTimeThreshold);
+    int ms = RunNetAndGetDuration(string(kSleepNetDefStringReadAfterRead), "dag");
+    EXPECT_NEAR(ms, 250, kTimeThreshold);
 }
 
 // For sanity check, we also test the sequential time - it should take 0.35
 // seconds instead since everything has to be sequential.
 TEST(SimpleNetTest, TestSimpleNetTimingReadAfterRead) {
-  int ms =
-      RunNetAndGetDuration(string(kSleepNetDefStringReadAfterRead), "simple");
-  EXPECT_NEAR(ms, 350, kTimeThreshold);
+    int ms =
+        RunNetAndGetDuration(string(kSleepNetDefStringReadAfterRead), "simple");
+    EXPECT_NEAR(ms, 350, kTimeThreshold);
 }
 
 // This network has two operators writing out the sleep2 blob. As a result, the
@@ -200,15 +200,15 @@ const char kSleepNetDefStringWriteAfterWrite[] =
     "  }";
 
 TEST(DAGNetTest, TestDAGNetTimingWriteAfterWrite) {
-  int ms =
-      RunNetAndGetDuration(string(kSleepNetDefStringWriteAfterWrite), "dag");
-  EXPECT_NEAR(ms, 350, kTimeThreshold);
+    int ms =
+        RunNetAndGetDuration(string(kSleepNetDefStringWriteAfterWrite), "dag");
+    EXPECT_NEAR(ms, 350, kTimeThreshold);
 }
 
 TEST(SimpleNetTest, TestSimpleNetTimingWriteAfterWrite) {
-  int ms =
-      RunNetAndGetDuration(string(kSleepNetDefStringWriteAfterWrite), "simple");
-  EXPECT_NEAR(ms, 350, kTimeThreshold);
+    int ms =
+        RunNetAndGetDuration(string(kSleepNetDefStringWriteAfterWrite), "simple");
+    EXPECT_NEAR(ms, 350, kTimeThreshold);
 }
 
 // This network has an operator writing to sleep1 while another operator is
@@ -248,15 +248,15 @@ const char kSleepNetDefStringWriteAfterRead[] =
     "  }";
 
 TEST(DAGNetTest, TestDAGNetTimingWriteAfterRead) {
-  int ms =
-      RunNetAndGetDuration(string(kSleepNetDefStringWriteAfterRead), "dag");
-  EXPECT_NEAR(ms, 350, kTimeThreshold);
+    int ms =
+        RunNetAndGetDuration(string(kSleepNetDefStringWriteAfterRead), "dag");
+    EXPECT_NEAR(ms, 350, kTimeThreshold);
 }
 
 TEST(SimpleNetTest, TestSimpleNetTimingWriteAfterRead) {
-  int ms =
-      RunNetAndGetDuration(string(kSleepNetDefStringWriteAfterRead), "simple");
-  EXPECT_NEAR(ms, 350, kTimeThreshold);
+    int ms =
+        RunNetAndGetDuration(string(kSleepNetDefStringWriteAfterRead), "simple");
+    EXPECT_NEAR(ms, 350, kTimeThreshold);
 }
 
 // This network has an operator writing to sleep1 while another
