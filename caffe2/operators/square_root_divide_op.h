e8d3da440e265108581641ede58e385fcb974a38
Restyled.io <commits@restyled.io>
2020-03-04 16:46:20 +0000

    Restyled by astyle

--- caffe2/operators/square_root_divide_op.h
+++ caffe2/operators/square_root_divide_op.h
@@ -9,52 +9,52 @@ namespace caffe2 {
 
 template <class Context>
 class SquareRootDivideOp final : public Operator<Context> {
- public:
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-  USE_DISPATCH_HELPER;
+public:
+    USE_OPERATOR_CONTEXT_FUNCTIONS;
+    USE_DISPATCH_HELPER;
 
-  template <class... Args>
-  explicit SquareRootDivideOp(Args&&... args)
-      : Operator<Context>(std::forward<Args>(args)...) {}
+    template <class... Args>
+    explicit SquareRootDivideOp(Args&&... args)
+        : Operator<Context>(std::forward<Args>(args)...) {}
 
-  bool RunOnDevice() override {
-    return DispatchHelper<TensorTypes<float>>::call(this, Input(DATA));
-  }
-
- private:
-  template <typename TData>
-  bool DoRunWithType() {
-    return DispatchHelper<TensorTypes2<float, int32_t, int64_t>, TData>::call(
-        this, Input(SCALE));
-  }
-
-  template <typename TData, typename TScale>
-  bool DoRunWithType2() {
-    auto& data = Input(DATA);
-    auto& scale = Input(SCALE);
-
-    auto* Y = Output(0, data.sizes(), at::dtype<TData>());
-    size_t batchSize = data.size(0);
-    size_t exampleSize = data.size_from_dim(1);
-    CAFFE_ENFORCE(batchSize == scale.size(0), batchSize, " != ", scale.size(0));
-    auto* scalePtr = scale.template data<TScale>();
-    auto* dataPtr = data.template data<TData>();
-    auto* yPtr = Y->template mutable_data<TData>();
-    for (auto i = 0U; i < batchSize; ++i) {
-      auto scale = scalePtr[i];
-      CAFFE_ENFORCE(scale >= 0, scale, " < 0");
-      auto multiplier = scale == 0 ? 1.0 : 1 / std::sqrt(scale);
-      math::Scale<float, TData, Context>(
-          exampleSize,
-          multiplier,
-          dataPtr + i * exampleSize,
-          yPtr + i * exampleSize,
-          &context_);
+    bool RunOnDevice() override {
+        return DispatchHelper<TensorTypes<float>>::call(this, Input(DATA));
     }
-    return true;
-  }
 
-  INPUT_TAGS(DATA, SCALE);
+private:
+    template <typename TData>
+    bool DoRunWithType() {
+        return DispatchHelper<TensorTypes2<float, int32_t, int64_t>, TData>::call(
+                   this, Input(SCALE));
+    }
+
+    template <typename TData, typename TScale>
+    bool DoRunWithType2() {
+        auto& data = Input(DATA);
+        auto& scale = Input(SCALE);
+
+        auto* Y = Output(0, data.sizes(), at::dtype<TData>());
+        size_t batchSize = data.size(0);
+        size_t exampleSize = data.size_from_dim(1);
+        CAFFE_ENFORCE(batchSize == scale.size(0), batchSize, " != ", scale.size(0));
+        auto* scalePtr = scale.template data<TScale>();
+        auto* dataPtr = data.template data<TData>();
+        auto* yPtr = Y->template mutable_data<TData>();
+        for (auto i = 0U; i < batchSize; ++i) {
+            auto scale = scalePtr[i];
+            CAFFE_ENFORCE(scale >= 0, scale, " < 0");
+            auto multiplier = scale == 0 ? 1.0 : 1 / std::sqrt(scale);
+            math::Scale<float, TData, Context>(
+                exampleSize,
+                multiplier,
+                dataPtr + i * exampleSize,
+                yPtr + i * exampleSize,
+                &context_);
+        }
+        return true;
+    }
+
+    INPUT_TAGS(DATA, SCALE);
 };
 
 } // namespace caffe2
