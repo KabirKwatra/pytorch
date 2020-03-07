ebb29f2d34772030f96a869acee78782387594b5
Restyled.io <commits@restyled.io>
2020-03-07 02:46:30 +0000

    Restyled by clang-format

--- aten/src/ATen/native/quantized/cpu/quantized_ops.h
+++ aten/src/ATen/native/quantized/cpu/quantized_ops.h
@@ -6,109 +6,119 @@ namespace at {
 namespace native {
 
 using qrelu_fn = void (*)(const at::Tensor& /*qx*/, at::Tensor& /*qy*/);
-using qrelu_leaky_fn = void (*)(Tensor& /*out*/, const Tensor& /*qx*/,
-                                Scalar /*negval_*/);
+using qrelu_leaky_fn =
+    void (*)(Tensor& /*out*/, const Tensor& /*qx*/, Scalar /*negval_*/);
 using qsigmoid_fn = void (*)(const at::Tensor& /*qx*/, at::Tensor& /*qy*/);
 using qclamp_fn = void (*)(
-                      const at::Tensor& /*qx*/,
-                      Scalar min,
-                      Scalar max,
-                      at::Tensor& /*qy*/);
+    const at::Tensor& /*qx*/,
+    Scalar min,
+    Scalar max,
+    at::Tensor& /*qy*/);
 using qtanh_fn = void (*)(const at::Tensor& /*qx*/, at::Tensor& /*qy*/);
 using qadd_fn =
     void (*)(Tensor& /*out*/, const Tensor& /*self*/, const Tensor& /*other*/);
 using qmaxpool_2d_fn = void (*)(
-                           const Tensor& qx,
-                           int64_t iC, // input/output channels
-                           int64_t iH,
-                           int64_t iW, // input sizes
-                           int64_t oH,
-                           int64_t oW, // output sizes
-                           int64_t kH,
-                           int64_t kW, // kernel size
-                           int64_t sH,
-                           int64_t sW, // strides
-                           int64_t pH,
-                           int64_t pW, // padding
-                           int64_t dH,
-                           int64_t dW, // dilation
-                           Tensor& qy);
+    const Tensor& qx,
+    int64_t iC, // input/output channels
+    int64_t iH,
+    int64_t iW, // input sizes
+    int64_t oH,
+    int64_t oW, // output sizes
+    int64_t kH,
+    int64_t kW, // kernel size
+    int64_t sH,
+    int64_t sW, // strides
+    int64_t pH,
+    int64_t pW, // padding
+    int64_t dH,
+    int64_t dW, // dilation
+    Tensor& qy);
 using qadaptive_avg_pool2d_fn = void (*)(
-                                    const Tensor& qx,
-                                    Tensor& qy,
-                                    int64_t b,
-                                    int64_t sizeC,
-                                    int64_t isizeH,
-                                    int64_t isizeW,
-                                    int64_t osizeH,
-                                    int64_t osizeW,
-                                    int64_t istrideB,
-                                    int64_t istrideC,
-                                    int64_t istrideH,
-                                    int64_t istrideW);
+    const Tensor& qx,
+    Tensor& qy,
+    int64_t b,
+    int64_t sizeC,
+    int64_t isizeH,
+    int64_t isizeW,
+    int64_t osizeH,
+    int64_t osizeW,
+    int64_t istrideB,
+    int64_t istrideC,
+    int64_t istrideH,
+    int64_t istrideW);
 
 using qavg_pool2d_fn = void (*)(
-                           const Tensor& qx,
-                           Tensor& qy,
-                           int64_t b,
-                           int64_t nInputPlane,
-                           int64_t inputWidth,
-                           int64_t inputHeight,
-                           int64_t outputWidth,
-                           int64_t outputHeight,
-                           int kW,
-                           int kH,
-                           int dW,
-                           int dH,
-                           int padW,
-                           int padH,
-                           bool count_include_pad,
-                           c10::optional<int64_t> divisor_override);
+    const Tensor& qx,
+    Tensor& qy,
+    int64_t b,
+    int64_t nInputPlane,
+    int64_t inputWidth,
+    int64_t inputHeight,
+    int64_t outputWidth,
+    int64_t outputHeight,
+    int kW,
+    int kH,
+    int dW,
+    int dH,
+    int padW,
+    int padH,
+    bool count_include_pad,
+    c10::optional<int64_t> divisor_override);
 
 using qavg_pool3d_fn = void (*)(
-                           const Tensor& qx,
-                           Tensor& qy,
-                           int64_t b,
-                           int64_t nInputPlane,
-                           int64_t inputWidth,
-                           int64_t inputHeight,
-                           int64_t inputDepth,
-                           int64_t outputWidth,
-                           int64_t outputHeight,
-                           int64_t outputDepth,
-                           int kW,
-                           int kH,
-                           int kD,
-                           int dW,
-                           int dH,
-                           int dD,
-                           int padW,
-                           int padH,
-                           int padD,
-                           bool count_include_pad,
-                           c10::optional<int64_t> divisor_override);
+    const Tensor& qx,
+    Tensor& qy,
+    int64_t b,
+    int64_t nInputPlane,
+    int64_t inputWidth,
+    int64_t inputHeight,
+    int64_t inputDepth,
+    int64_t outputWidth,
+    int64_t outputHeight,
+    int64_t outputDepth,
+    int kW,
+    int kH,
+    int kD,
+    int dW,
+    int dH,
+    int dD,
+    int padW,
+    int padH,
+    int padD,
+    bool count_include_pad,
+    c10::optional<int64_t> divisor_override);
 
 using qupsample_bilinear2d_fn = void (*)(
-                                    Tensor& output,
-                                    const Tensor& input,
-                                    int64_t input_height,
-                                    int64_t input_width,
-                                    int64_t output_height,
-                                    int64_t output_width,
-                                    int64_t nbatch,
-                                    int64_t channels,
-                                    bool align_corners,
-                                    c10::optional<double> scales_h,
-                                    c10::optional<double> scales_w);
+    Tensor& output,
+    const Tensor& input,
+    int64_t input_height,
+    int64_t input_width,
+    int64_t output_height,
+    int64_t output_width,
+    int64_t nbatch,
+    int64_t channels,
+    bool align_corners,
+    c10::optional<double> scales_h,
+    c10::optional<double> scales_w);
 
 using qcat_nhwc_fn = Tensor (*)(
-                         const c10::List<Tensor>& qxs,
-                         int64_t dim,
-                         double scale,
-                         int64_t zero_point);
-using qtopk_fn = void(*)(Tensor&, Tensor&, const Tensor&, int64_t, int64_t, bool, bool);
+    const c10::List<Tensor>& qxs,
+    int64_t dim,
+    double scale,
+    int64_t zero_point);
+using qtopk_fn =
+    void (*)(Tensor&, Tensor&, const Tensor&, int64_t, int64_t, bool, bool);
 
-using qbatch_norm_fn = void(*)(int64_t, int64_t, int64_t, int64_t, int64_t, const Tensor&, const Tensor&, const Tensor&, Tensor&);
+using qbatch_norm_fn = void (*)(
+    int64_t,
+    int64_t,
+    int64_t,
+    int64_t,
+    int64_t,
+    const Tensor&,
+    const Tensor&,
+    const Tensor&,
+    Tensor&);
 
 // using qavg_pool2d_fn
 DECLARE_DISPATCH(qrelu_fn, qrelu_stub);
